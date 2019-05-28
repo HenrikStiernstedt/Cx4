2a9da0484df483f09555ecf0645ebc55febf6103
Henrik Stiernstedt <henrik@stiernstedt.net>
2019-05-28 19:45:59 +0200

    Snyggar till visningen

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 function Square(props) {
   return (
     <button  className="square" style={{color: props.color}} onClick={props.onClick}>
-      {props.value} {props.color}
+      {props.value}
     </button>
   );
 }
